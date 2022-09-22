887680c7a4d4ef5661810d9469d71f4f3fcbf87f
Baz <bazilkhn@gmail.com>
2022-09-22 07:00:25 +0000

    Adds skeleton structure for main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav>Ad Manager</nav>
-        <p>yo</p>
-        <p>test</p>
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">
+                WebSiteName
+              </a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active">
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">My Ads</a>
+              </li>
+              <li>
+                <a href="#">Create Ad</a>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <a href="#">
+                  <span class="glyphicon glyphicon-user"></span> Sign Up
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="glyphicon glyphicon-log-in"></span> Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <body>
+        <h1>Welcome user!</h1>
+        <button type="button">Click Me!</button>
+        <h3>View my ads</h3>
+      </body>
     </div>
   );
 }
