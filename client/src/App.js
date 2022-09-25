5afd1dc2b959295eea5c0cdd6736a50f71aeb91f
Baz <bazilkhn@gmail.com>
2022-09-25 22:51:33 +0000

    Adds navbar component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,25 @@
-import "./App.css";
+// import "./App.css";
 import React from "react";
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+const switchListing = function switchToListing() {
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <nav class="navbar navbar-inverse">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="#">
-                WebSiteName
-              </a>
-            </div>
-            <ul class="nav navbar-nav">
-              <li class="active">
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">My Ads</a>
-              </li>
-              <li>
-                <a href="#">Create Ad</a>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li>
-                <a href="#">
-                  <span class="glyphicon glyphicon-user"></span> Sign Up
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span class="glyphicon glyphicon-log-in"></span> Login
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+      <Navbar />
+      {/* <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Router path="/" exact/>
+        </Routes>
+      </Router> */}
       <body>
         <h1>Welcome user!</h1>
-        <button type="button">Click Me!</button>
+        <button type="button">Create Ad</button>
         <h3>View my ads</h3>
       </body>
     </div>
