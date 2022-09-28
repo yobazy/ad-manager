7ac614289963a8ac6bbde75f1d39da7964236b76
Baz <king.bazil.khan@gmail.com>
2022-09-28 07:29:35 +0000

    Adds photo uploading, formats AdForm

--- client/src/components/AdForm.js
+++ client/src/components/AdForm.js
@@ -11,41 +11,44 @@ export default class App extends React.Component {
         <div>
             <h2>Create new listing</h2>
             <form>
-                <label>
-                    Photos add up to 10:
-                    <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
+                <label for="img">Photos (add up to 10):</label><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
+                    <input type="file" id="img" name="img" accept="image/*" /><br></br>
                 <label>
                     Title:
                     <input type="text" name="name" required/>
-                </label>
-                <input type="submit" value="Submit" />
+                </label><br></br>
                 <label>
                     Price:
                     <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
+                </label><br></br>
                 <label>
                     Category:
                     <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
+                </label><br></br>
                 <label>
                     Condition:
-                </label>
-                <select
-                // value={this.state.value} 
-                // onChange={this.handleChange}
-                >
+                    <select
+                        // value={this.state.value} 
+                        // onChange={this.handleChange}
+                    >
                     <option value="grapefruit">New</option>
                     <option value="lime">Used - good</option>
                     <option value="coconut">Used - like new</option>
-                </select>                
+                </select>
+                </label><br></br> 
+                    
                 <label>
                     Description:
-                    <input type="text" name="name" />
-                </label>
+                    <textarea></textarea>
+                </label><br></br>
                 <input type="submit" value="Submit" />
 
             </form>
