543899fe862995de9066f871deecf684f0b57799
Baz <king.bazil.khan@gmail.com>
2022-10-06 09:06:57 +0000

    Updates navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,22 +15,27 @@ export default function Navbar() {
     return  (
             <nav className="navbar">
                 <div className="navbar-container">
-                    <a href="/" className="navbar-logo">AdMan</a>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    <div className="">
+                        <a href="/" className="nav-links">AdMan</a>
+                        <div className="menu-icon" onClick={handleClick}>
+                            <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                        </div>
                     </div>
                     <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
                         <li className="nav-item">
                             <a href="/" className="nav-links" onClick={closeMobileMenu}>Home</a>
                         </li>
                         <li className="nav-item">
-                            <a href="/newad" className="nav-links" onClick={closeMobileMenu}>Create Ad</a>
+                            <a href="/create" className="nav-links" onClick={closeMobileMenu}>Create Ad</a>
                         </li>
                         <li className="nav-item">
-                            <a href="/viewads" className="nav-links" onClick={closeMobileMenu}>View Ad</a>
+                            <a href="/view" className="nav-links" onClick={closeMobileMenu}>View Ad</a>
                         </li>
                         <li className="nav-item">
                             <a href="/sign-up" className="nav-links" onClick={closeMobileMenu}>Sign Up</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/sign-up" className="nav-links" onClick={closeMobileMenu}>Test ERR!</a>
                         </li>
                     </ul>
                     
