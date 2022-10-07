71405ba43cf0a982f2b3602f4b956ab2963c52ca
Baz <king.bazil.khan@gmail.com>
2022-10-07 22:09:31 +0000

    Updates styling and layout of create page

--- client/src/routes/Create.js
+++ client/src/routes/Create.js
@@ -5,6 +5,8 @@ import Footer from '../components/Footer';
 import AdForm from '../components/AdForm';
 import ViewAds from '../components/pages/ViewAds';
 // import './App.css'
+import './Create.css'
+
 
 function Create(props) {
     // const [adsInfo, setAdsInfo] = useState([{
@@ -27,13 +29,13 @@ function Create(props) {
 
     
     return (
-        <div className="App">
-            <body>
-                    <p>Create new listing</p>
-                    <p>You can create a new listing using the form below</p>
-                    <AdForm ads={props.adsInfo} func={props.setAdsInfo}></AdForm>
-                    <ViewAds listings={props.adsInfo}></ViewAds>
-            </body>
+        <div className="create-container">
+            <div className="create-information">
+                <h1>Create new listing</h1>
+                <p>You can create a new listing using the form below</p>
+            </div>
+                <AdForm ads={props.adsInfo} func={props.setAdsInfo}></AdForm>
+                <ViewAds listings={props.adsInfo}></ViewAds>
         </div>
     );
 }
