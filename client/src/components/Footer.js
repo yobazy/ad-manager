c22d97f46d3d777fb05932ab7ef0434a273b5cce
Baz <king.bazil.khan@gmail.com>
2022-10-07 21:18:11 +0000

    Updates footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,34 +8,28 @@ function Footer() {
                 <p>Thanks for using AdMan!</p>
             </section>
             <div className="container">
-          <div className="down-below">
-            <div className="contact">
+          <div className="contact-information">
+            <div className="email-contact">
               <a id="email" href="mailto:bazilkhn@gmail.com" title="Email me">
                 bazilkhn@gmail.com
               </a>
             </div>
-            <ul className="links">
-              <li>
-                <a
-                  href="https://github.com/youthbazzy"
-                  title="@youthbazzy on GitHub"
-                >
+            <ul className="social-links">
+              <li className="social-link">
+                <a href="https://github.com/youthbazzy" title="@youthbazzy on GitHub">
                   <i className="fa fa-github fa-xl" />
                 </a>
               </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/bazil-k-9592a7b6/"
-                  title="Bazil Khan's resume on LinkedIn"
-                >
+              <li className="social-link">
+                <a href="https://www.linkedin.com/in/bazil-k-9592a7b6/" title="Bazil Khan on LinkedIn">
                   <i className="fa fa-linkedin" />
                 </a>
               </li>
             </ul>
-            <div className="info">
+          </div>
+          <div className="copyright-info">
               <p>Copyright © Bazil Khan</p>
             </div>
-          </div>
         </div>
         </div>
     )
