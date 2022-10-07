49856634ad2cd065218cd36d68dd8e9d5c5f0402
Baz <king.bazil.khan@gmail.com>
2022-10-07 21:44:59 +0000

    Adds images to home page

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import './Home.css'
+import ad2 from '../images/ad2.png';
+import ad3 from '../images/ad3.png';
+
+// import ad2 from 'client/src/images/ad2.png';
+
 
 function Home() {
     return (
@@ -12,8 +17,8 @@ function Home() {
                 <h1 className="text">Welcome user!</h1>
                 <p className="text">Please log in to view your ads and create new postings that can be posted on facebook marketplace and kijiji.</p>
                 <div className="images-container">
-                    <img className="example-ad" alt="example"></img>
-                    <img className="example-ad" alt="example"></img>
+                    <img src={ad2} className="example-ad" alt="example"></img>
+                    <img src={ad3} className="example-ad" alt="example"></img>
                 </div>
                 <div className="home-buttons">
                     <button className="home-button">Log in</button>
