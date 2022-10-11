fdc637bc86d7b3f316770730eb1a7ebf02d36c4e
Baz <king.bazil.khan@gmail.com>
2022-10-11 07:00:06 +0000

    Cleans up footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './Footer.css';
+import { faRocket, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 
 function Footer() {
     return (
@@ -14,18 +18,18 @@ function Footer() {
                 bazilkhn@gmail.com
               </a>
             </div>
-            <ul className="social-links">
+            <section className="social-links">
               <li className="social-link">
                 <a href="https://github.com/youthbazzy" title="@youthbazzy on GitHub">
-                  <i className="fa fa-github fa-xl" />
+                  <FontAwesomeIcon icon={faRocket}/>
                 </a>
               </li>
               <li className="social-link">
                 <a href="https://www.linkedin.com/in/bazil-k-9592a7b6/" title="Bazil Khan on LinkedIn">
-                  <i className="fa fa-linkedin" />
+                <FontAwesomeIcon icon={faCoffee}/>
                 </a>
               </li>
-            </ul>
+            </section>
           </div>
           <div className="copyright-info">
               <p>Copyright © Bazil Khan</p>
