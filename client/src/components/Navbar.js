1295365a71e49df30feebede06730e3d7364f582
Baz <king.bazil.khan@gmail.com>
2022-10-11 06:52:40 +0000

    Updates navbar styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,36 +16,37 @@ export default function Navbar() {
     return  (
             <nav className="navbar">
                 <div className="navbar-container">
-                    <div className="logo-name">
+                    <div className="logo-name nav-items">
                         <img src={logo} width={40} alt="logo"></img>
                         <a href="/" className="nav-links">AdMan</a>
                         <div className="menu-icon" onClick={handleClick}>
                             <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
                         </div>
                     </div>
-                    <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
-                        <li className="nav-item">
-                            <a href="/" className="nav-links" onClick={closeMobileMenu}>Home</a>
-                        </li>
-                        {/* The following should show when logged in */}
-                        <li className="nav-item">
-                            <a href="/create" className="nav-links" onClick={closeMobileMenu}>Create Ad</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/view" className="nav-links" onClick={closeMobileMenu}>View Ad</a>
-                        </li>
-                        <li className="nav-item nav-log">
-                            <a href="/logout" className="nav-links" onClick={closeMobileMenu}>Log Out</a>
-                        </li>
-                        {/* The following should show when not logged in */}
-                        <li className="nav-item">
-                            <a href="/login" className="nav-links" onClick={closeMobileMenu}>Log In</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/signup" className="nav-links" onClick={closeMobileMenu}>Sign Up</a>
-                        </li>
-                    </ul>
-                    
+                    <div className="nav-items">
+                        <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                            <li className="nav-item">
+                                <a href="/" className="nav-links" onClick={closeMobileMenu}>Home</a>
+                            </li>
+                            {/* The following should show when logged in */}
+                            <li className="nav-item">
+                                <a href="/create" className="nav-links" onClick={closeMobileMenu}>Create Ad</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/view" className="nav-links" onClick={closeMobileMenu}>View Ad</a>
+                            </li>
+                            <li className="nav-item nav-log">
+                                <a href="/logout" className="nav-links" onClick={closeMobileMenu}>Log Out</a>
+                            </li>
+                            {/* The following should show when not logged in */}
+                            <li className="nav-item">
+                                <a href="/login" className="nav-links" onClick={closeMobileMenu}>Log In</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/signup" className="nav-links" onClick={closeMobileMenu}>Sign Up</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>    
             )
